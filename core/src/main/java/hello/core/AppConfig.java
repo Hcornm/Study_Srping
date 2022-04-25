a51394d5cfba143298f7e7a9f463af4f8d741338
hmlee <corn202023@gmail.com>
2022-04-25 23:13:17 +0900

    스프링빈 테스트 코드
    스프링빈 테스트 코드

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,15 +9,20 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepositoy;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // AppConfig의 사용으로 서비스 로진 변경 시 사용영역에 있는 코드들을 수정할 필요 없이 (구현체, 추상화)
 // 구성영역인 AppConfig만 수정해주면 된다.
 // OCP를 위반하지 않는다.
+
+@Configuration
 public class AppConfig {
 
     // 사용할 구현체를 리턴
     // 실제로 멤모리멤버 리포지토리에서 디비멤버리포지토리로 바꿔야하면 return 해주는 부분만 변경해주면
     // 소스 전체를 수정할 필요없이 서비스가 디비멤버리포지토리로 변경됨
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepositoy();
     }
@@ -25,17 +30,20 @@ public class AppConfig {
     // 사용할 구현체를 리턴
     // 마찬가지로 할인정책을 변경하고 싶으면 return 부분만 변경해주면된다.
     // RateDiscountPolicy
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 
     // 구현체를 가지고있는 인터페이스를 호출
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
     // 구현체를 가지고있는 인터페이스를 호출
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -7,16 +7,32 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 
-//        MemberService memberService = new MemberServiceImpl();
-//        OrderService orderService = new OrderServiceImpl();
+//        ApplicationContext를 스프링 컨테이너라고한다.
+//        스프링 컨테이너는 @Configuration 어노테이션이 붙은 AppConfig.java를 설정 정보로 사용한다.
+//        여기서 @Bean이라고 적힌 메서드를 모두 호출해서 반환된 객체를 스프링 컨테이너에 등록한다. 이렇게 스프링 컨테이너에 등록된 객체를 스프링 빈이라고 한다.
+//
+//        이전에는 개발자가 직접 필요한 객체를 AppConfig를 사용해서 직접 조회했지만 이제부터는 스프링컨테이너를 통해서 필요한 스프링 빈을 찾아야한다.
+//        필요한 스프링 빈을 찾는 방법은 applicationContext.getBean()메서드를 사용하며 첫 번째 인자값으로는 @Bean으로 등록한 객체의 이름을(일치해야함),
+//        두 번째 인자값으로는 반환 타입을 넣어주면 된다.
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
+
+
+
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
