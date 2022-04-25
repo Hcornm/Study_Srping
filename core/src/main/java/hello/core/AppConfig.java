02e952d7d0ffcfe090f4b16bc18c614ead730a1d
hmlee <corn202023@gmail.com>
2022-04-25 21:39:58 +0900

    AppConfig 주석 추가
    AppConfig 주석 추가

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -9,6 +10,9 @@ import hello.core.member.MemoryMemberRepositoy;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+// AppConfig의 사용으로 서비스 로진 변경 시 사용영역에 있는 코드들을 수정할 필요 없이 (구현체, 추상화)
+// 구성영역인 AppConfig만 수정해주면 된다.
+// OCP를 위반하지 않는다.
 public class AppConfig {
 
     // 사용할 구현체를 리턴
@@ -22,7 +26,8 @@ public class AppConfig {
     // 마찬가지로 할인정책을 변경하고 싶으면 return 부분만 변경해주면된다.
     // RateDiscountPolicy
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
     // 구현체를 가지고있는 인터페이스를 호출
