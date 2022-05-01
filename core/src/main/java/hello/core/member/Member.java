6c3f7292f911e0e4cd413e42135a1ad25cb3dc82
hmlee <corn202023@gmail.com>
2022-05-01 16:16:08 +0900

    Lombok 라이브러리
    Lombok 라이브러리

--- core/src/main/java/hello/core/member/Member.java
+++ core/src/main/java/hello/core/member/Member.java
@@ -1,5 +1,24 @@
 package hello.core.member;
 
+
+import lombok.*;
+
+// lombok 라이브러리를 사용해서 노가다 작업을 줄일 수 있다.
+// lombok 라이브러리가 자동으로 필드값들에 대한 Getter 메서드를 생성해준다.
+// 클래스 위에 어노테이션을 사용시 : 클래스에 해당하는 모든 필드들 해당
+// 필드값 위에 직접 @Getter 사용시: 해당 필드만 Getter 사용
+@Getter
+// Setter 메서드 생성
+@Setter
+// 파라미터가 없는 기본 생성자 생성
+//@NoArgsConstructor
+// 모든 필드값을 파리미터로 받는 생성자 생성
+//@AllArgsConstructor
+// final이나 @NonNull인 필드 값만 파라미터로 받는 생성자 생성
+//@RequiredArgsConstructor
+
+// 위의 모든 어노테이션을 한방에 설정 ㅡㅡ
+// @Data
 public class Member {
 
     // id
@@ -16,28 +35,28 @@ public class Member {
         this.grade = grade;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Grade getGrade() {
-        return grade;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setGrade(Grade grade) {
-        this.grade = grade;
-    }
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public Grade getGrade() {
+//        return grade;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setGrade(Grade grade) {
+//        this.grade = grade;
+//    }
 
 }
--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,16 @@
 package hello.core.member;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 // 구현체가 하나만 있을 때는 인터페이스명 뒤에 Impl을 븥이는 게 관례
 @Component
+
+// final이 붙은 객체의 생성자를 자동으로 만들어줌
+// 생성자 방식으로 주입을 한다면 lombok의 @RequiredArgsConstructor 어노테이션으로
+// 의존관계 주입을 편하게 할 수 있다. --> 아주 중요
+@RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
     // 서비스 구현체가 멤버를 찾고 가입시킬려면 MemberRepository가 필요하다.
@@ -16,11 +22,11 @@ public class MemberServiceImpl implements MemberService {
     // 이러면 추상화에만 의존한다
     private final MemberRepository memberRepository;
 
+    //@RequiredArgsConstructor 사용으로 주석처리
     // 생성자 주입
-    @Autowired // = ac.getBean(MemberRepository.class)
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+//    @Autowired // = ac.getBean(MemberRepository.class)
+//        this.memberRepository = memberRepository;
+//    }
 
     @Override
     public void join(Member member) {
