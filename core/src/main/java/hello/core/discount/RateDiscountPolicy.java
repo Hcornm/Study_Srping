b7014980fb88e1adff6ff31e3c3faaaf36974930
hmlee <corn202023@gmail.com>
2022-05-02 23:05:11 +0900

    조회될 빈이 2개 이상일 시
    조회될 빈이 2개 이상일 시

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,9 +2,13 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+
+// 생성자 주입 시 조회되는 빈이 2개 이상일시 @Primay가 붙은 빈을 디폴트로 등록한다. (우선순위)
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
