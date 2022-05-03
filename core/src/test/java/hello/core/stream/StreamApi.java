35444098c56319f715fc147855d1c27984bb3b8f
hmlee <corn202023@gmail.com>
2022-05-04 00:17:26 +0900

    Stream API
    Stream API

--- core/src/test/java/hello/core/stream/StreamApi.java
+++ core/src/test/java/hello/core/stream/StreamApi.java
@@ -37,6 +37,16 @@ public class StreamApi {
         System.out.println("isTest = " + isTest);
 
 
+        int[] intArr = {2, 4, 6};
+        boolean result = Arrays.stream(intArr).allMatch(a -> a%2 == 0);
+        System.out.println("2의 배수? " + result);
+        result = Arrays.stream(intArr).anyMatch(a -> a%3 == 0);
+        System.out.println("3의 배수가 하나라도 있나? " + result);
+        result = Arrays.stream(intArr).noneMatch(a -> a%3 == 0);
+        System.out.println("3의 배수가 없나? " + result);
+
+
+
 
 //            List<String> oldList = Arrays.asList{"1", "2", "3", "4"};
 //            List<String> newList = Arrays.asList{"3", "4", "5", "6"};
